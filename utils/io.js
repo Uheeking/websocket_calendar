f67dc9d4cac7253f12bc315a382e86708a0de4ef
Uheeking <wert1847@naver.com>
2023-10-19 22:34:05 +0900

    ADD : 컨트롤러 추가 및 프론트 수정

--- utils/io.js
+++ utils/io.js
@@ -1,9 +1,21 @@
+const userController = require("../Controllers/user.controller");
+
 module.exports = function (io) {
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
-  });
 
-  socket.on("disconnect", () => {
-    console.log("user is disconnect");
-  })
+    socket.on("login", async (userName, cb) => {
+      try {
+        console.log("backend", userName);
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: true, error: error.message });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user is disconnect");
+    });
+  });
 };
