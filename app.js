ce65e179db7036d7e0996941893d6eb62ab7dc19
Uheeking <wert1847@naver.com>
2023-10-19 15:43:10 +0900

    ADD : 프론트 기능 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.DB,{
     useNewUrlParser: true,
