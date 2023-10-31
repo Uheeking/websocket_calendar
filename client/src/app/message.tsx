dc55747664843819a99ac3cf9fae6fd538eca0fe
Uheeking <wert1847@naver.com>
2023-10-31 21:56:25 +0900

    ADD : 채팅창 메시지 보이기 기능 구현

---
+++ client/src/app/message.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import InputField from "./inputField";
+import socket from "./server";
+
+const MessageContainer = ({ messageList, user }) => {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    socket.emit("sendMessage", message, (res) => {
+      console.log("send mess", res);
+    });
+  };
+
+  return (
+    <main className="flex-1 p-4 flex justify-center">
+      <div className="mx-auto bg-white rounded-xl shadow-md overflow-hidden w-full sm:w-4/5 md:w-3/4 lg:w-full xl:w-full">
+        <div className="flex justify-between p-3 border-b">
+          <h2 className="text-lg font-medium">Chat</h2>
+          <svg
+            className="h-4 w-4 text-gray-600"
+            fill="none"
+            height="24"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+            <path d="M21 3v5h-5" />
+            <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
+            <path d="M8 16H3v5" />
+          </svg>
+        </div>
+        <div className="p-3 h-3/4 overflow-y-scroll">
+          {messageList.map((message, index) => (
+            <div
+              key={index}
+              className={`flex items-center space-x-3 p-2 ${
+                message.user.name === user.name ? "justify-end" : ""
+              }`}
+            >
+                {message.user.name === "system" ? (<>
+              <p
+                className={`rounded-md py-2 px-3 text-sm ${
+                  message.user.name === user.name
+                    ? "bg-blue-300"
+                    : "bg-gray-300"
+                }`}
+              >
+                {message.chat}
+              </p>
+              <img
+                alt={message.user.name}
+                className="rounded-full"
+                height="32"
+                src="./placeholder.png"
+                style={{
+                  aspectRatio: "32/32",
+                  objectFit: "cover",
+                }}
+                width="32"
+              /></>) : (<>
+              <img
+                  alt={message.user.name}
+                  className="rounded-full"
+                  height="32"
+                  src="./placeholder.png"
+                  style={{
+                    aspectRatio: "32/32",
+                    objectFit: "cover",
+                  }}
+                  width="32"
+                />
+                <p
+                  className={`rounded-md py-2 px-3 text-sm ${
+                    message.user.name === user.name
+                      ? "bg-blue-300"
+                      : "bg-gray-300"
+                  }`}
+                >
+                  {message.chat}
+                </p>
+                </>)}
+            </div>
+          ))}
+        </div>
+        <InputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default MessageContainer;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,13 +5,12 @@ import "./page.css";
 import socket from "./server";
 import axios from "axios";
 import TodoList from "./todo";
-import InputField from "./inputField";
+import Message from "./message";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
-export default function Component() {
+export default function Page() {
   const [date, setDate] = useState(new Date());
-  const [message, setMessage] = useState("");
   const [day, setDay] = useState([]);
   const [user, setUser] = useState();
   const [messageList, setMessageList] = useState([]);
@@ -28,7 +27,7 @@ export default function Component() {
         console.error("Error fetching data:", error);
       });
     socket.on("message", (message) => {
-      setMessageList((prevState)=>prevState.concat(message))
+      setMessageList((prevState) => prevState.concat(message));
     });
     const userName = prompt("당신의 이름을 입력하세요.");
     console.log("user", userName);
@@ -41,123 +40,12 @@ export default function Component() {
     });
   }, []);
 
-  const sendMessage = (event : any) => {
-    event.preventDefault();
-    socket.emit("sendMessage", message, (res:any) => {
-      console.log("send mess", res);
-    });
-  };
   return (
     <div
       key="1"
       className="containerStyle min-h-screen flex flex-col bg-gradient-to-r from-purple-200 to-blue-200"
     >
-      <div></div>
-      <main className="flex-1 p-4 flex justify-center">
-        <div className="mx-auto bg-white rounded-xl shadow-md overflow-hidden w-full sm:w-4/5 md:w-3/4 lg:w-full xl:w-full">
-          <div className="flex justify-between p-3 border-b">
-            <h2 className="text-lg font-medium">Chat</h2>
-            <svg
-              className=" h-4 w-4 text-gray-600"
-              fill="none"
-              height="24"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              width="24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-              <path d="M21 3v5h-5" />
-              <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
-              <path d="M8 16H3v5" />
-            </svg>
-          </div>
-          <div className="p-3 h-3/4 overflow-y-scroll">
-            <div className="flex items-center space-x-3 p-2">
-              <img
-                alt="Chat Bot"
-                className="rounded-full"
-                height="32"
-                src="/placeholder.png"
-                style={{
-                  aspectRatio: "32/32",
-                  objectFit: "cover",
-                }}
-                width="32"
-              />
-              <p className="bg-gray-300 rounded-md py-2 px-3 text-sm">
-                Hello! Let's start the test.
-              </p>
-            </div>
-            <div className="flex items-center justify-end space-x-3 p-2">
-              <p className="bg-blue-300 rounded-md py-2 px-3 text-sm">
-                Example chat from me 1.
-              </p>
-              <img
-                alt="Me"
-                className="rounded-full"
-                height="32"
-                src="./placeholder.png"
-                style={{
-                  aspectRatio: "32/32",
-                  objectFit: "cover",
-                }}
-                width="32"
-              />
-            </div>
-          </div>
-          <InputField
-            message={message}
-            setMessage={setMessage}
-            sendMessage={sendMessage}
-          />
-          {/* <div className="border-t p-3">
-              <div className="plus-button">+</div>
-              <form className="flex input-container">
-                <div className="flex-1">
-                  <Input
-                    placeholder="Type your answer..."
-                    value={message}
-                    onChange={(event) => setMessage(event.target.value)}
-                    multiline={false}
-                    rows={1}
-                    message={message}
-                    setMessage={setMessage}
-                    sendMessage={sendMessage}
-                  />
-                  <br />
-                  <Select>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select your answer" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Options</SelectLabel>
-                        <SelectItem value="option1">뭐해?</SelectItem>
-                        <SelectItem value="option2">놀자!</SelectItem>
-                        <SelectItem value="option3">만나자!</SelectItem>
-                        <SelectItem value="option4">내일 뭐해?</SelectItem>
-                        <SelectItem value="option5">잘 자~</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="">
-                  <Button
-                    className="send-button ml-2 h-full bg-gray-700 text-white text-sm"
-                    type="submit"
-                    disabled={message === ""}
-                  >
-                    Send
-                  </Button>
-                </div>
-              </form>
-            </div> */}
-        </div>
-      </main>
+      <Message messageList={messageList} user={user}/>
       <div className="calendar-container">
         <h2 className="text-lg font-medium m-3">Calendar</h2>
         <Calendar
